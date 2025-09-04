b968de611d55fda0785817b2fc5fb2b06904cf10
zzani0207 <87990208+zzani0207@users.noreply.github.com>
2025-09-04 16:16:38 +0900

    Initial commit

---
+++ ComputerGraphics/ComputerGraphics/HelloGL.cpp
@@ -0,0 +1,23 @@
+#include <vgl.h>
+
+void display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_TRIANGLES);
+		glColor3f(1.0f, 0.0f, 0.0f); glVertex2f(-0.5f, -0.5f);
+		glColor3f(0.0f, 1.0f, 0.0f); glVertex2f( 0.5f, -0.5f);
+		glColor3f(0.0f, 0.0f, 1.0f); glVertex2f( 0.0f,  0.5f);
+	glEnd();
+	glFlush();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize(400, 400);
+	glutCreateWindow("Hello OpenGL");
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
