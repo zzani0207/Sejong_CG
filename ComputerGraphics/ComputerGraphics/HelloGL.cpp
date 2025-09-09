d3003e115c6d19d899ed2f1b619dbe520a413925
zzani0207 <87990208+zzani0207@users.noreply.github.com>
2025-09-09 14:48:02 +0900

    HelloGL

--- ComputerGraphics/ComputerGraphics/HelloGL.cpp
+++ ComputerGraphics/ComputerGraphics/HelloGL.cpp
@@ -1,23 +1,58 @@
 #include <vgl.h>
+#include<math.h>
 
-void display(void)
+float dtheta = 0.0f;
+void display()
 {
+	glClearColor(1, 0, 0, 1); // Red background
+
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBegin(GL_TRIANGLES);
-		glColor3f(1.0f, 0.0f, 0.0f); glVertex2f(-0.5f, -0.5f);
-		glColor3f(0.0f, 1.0f, 0.0f); glVertex2f( 0.5f, -0.5f);
-		glColor3f(0.0f, 0.0f, 1.0f); glVertex2f( 0.0f,  0.5f);
+
+	//glColor3f(0.5, 0.5, 0.5); // Gray color
+
+	//glVertex2f(-0.5, -0.5); // 2f means 2 float arguments
+	//glVertex2f(0.5, -0.5);
+	//glVertex2f(-0.5, 0.5);
+
+	//glVertex2f(0.5, -0.5);
+	//glVertex2f(0.5, 0.5);
+	//glVertex2f(-0.5, 0.5);
+
+	int div = 100;
+	float r = 0.5;
+
+	for (int i = 0; i < div; i++)
+	{
+		glColor3f(0, 1.0f / div * i, 1 - 1.0f / div * i); // Gradient color
+
+		glVertex2f(0, 0);
+		float theta1 = 2 * 3.141592f / div * i + dtheta;
+		glVertex2f(r * cos(theta1), r * sin(theta1));
+		float theta2 = 2 * 3.141592f / div * (i + 1) + dtheta;
+		glVertex2f(r * cos(theta2), r * sin(theta2));
+	}
 	glEnd();
+
 	glFlush();
+
+	dtheta += 1 / (2 * 3.141592f);
 }
+
 
 int main(int argc, char** argv)
 {
+	//gluCreateWindow까진 초기세팅 디폴트 양식
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(400, 400);
-	glutCreateWindow("Hello OpenGL");
+
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	//해상도 설정
+	glutInitWindowSize(512, 512);
+	//창의 제목
+	glutCreateWindow("Hello GL");
+
 	glutDisplayFunc(display);
 	glutMainLoop();
+
 	return 0;
 }
