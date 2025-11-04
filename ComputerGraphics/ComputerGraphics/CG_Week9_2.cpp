f8397c0a6a86583f6abe0a201c3f8172309972d3
zzani0207 <87990208+zzani0207@users.noreply.github.com>
2025-11-04 14:35:57 +0900

    week9_2 robotarm2

--- ComputerGraphics/ComputerGraphics/CG_Week9_2.cpp
+++ ComputerGraphics/ComputerGraphics/CG_Week9_2.cpp
@@ -4,6 +4,8 @@
 #include "MyCube.h"
 #include "MyPyramid.h"
 
+#include <list>
+
 MyCube cube;
 MyPyramid pyramid;
 
@@ -24,6 +26,111 @@ void myInit()
 }
 
 mat4 CTM;
+void drawRobotArm1(float ang1, float ang2, float ang3)
+{
+	GLuint uMat = glGetUniformLocation(program, "uMat");
+
+	mat4 M(1.0);
+	mat4 CTM(1.0);
+
+	//Base
+	CTM = Translate(0, -0.5, 0);
+	M = Scale(0.3, 0.2, 0.2);
+	glUniformMatrix4fv(uMat, 1, true, CTM * M);
+	pyramid.Draw(program);
+
+	//Uupper Arm
+	CTM *= RotateZ(ang1);
+	M = Translate(0, 0.2, 0) * Scale(0.1, 0.4, 0.1);
+	glUniformMatrix4fv(uMat, 1, true, CTM * M);
+	cube.Draw(program);
+
+	//Lower Arm
+	CTM *= Translate(0, 0.4, 0) * RotateZ(ang2);
+	M = Translate(0, 0.2, 0) * Scale(0.1, 0.4, 0.1);
+	glUniformMatrix4fv(uMat, 1, true, CTM * M);
+	cube.Draw(program);
+
+	//Hand
+	CTM *= Translate(0, 0.4, 0) * RotateZ(ang3);
+	M = Translate(0, 0.05, 0) * Scale(0.2, 0.1, 0.1);
+	glUniformMatrix4fv(uMat, 1, true, CTM * M);
+	cube.Draw(program);
+}
+void drawRobotArm2(float ang1, float ang2, float ang3)
+{
+	GLuint uMat = glGetUniformLocation(program, "uMat");
+
+	mat4 M(1.0);
+	mat4 CTM(1.0);
+
+	std::list<mat4> MStack;
+	//Base
+	CTM = Translate(0, -0.5, 0);
+	M = Scale(0.3, 0.2, 0.2);
+	glUniformMatrix4fv(uMat, 1, true, CTM * M);
+	pyramid.Draw(program);
+
+	MStack.push_back(CTM);
+
+	//Uupper Arm
+	CTM *= RotateZ(ang1);
+	M = Translate(0, 0.2, 0) * Scale(0.1, 0.4, 0.1);
+	glUniformMatrix4fv(uMat, 1, true, CTM * M);
+	cube.Draw(program);
+
+	MStack.push_back(CTM);
+	//Lower Arm
+	CTM *= Translate(0, 0.4, 0) * RotateZ(ang2);
+	M = Translate(0, 0.05, 0) * Scale(0.4, 0.1, 0.1);
+	glUniformMatrix4fv(uMat, 1, true, CTM * M);
+	cube.Draw(program);
+
+	MStack.push_back(CTM);
+
+	//Hand1
+	CTM *= Translate(0.2, 0.1, 0) * RotateZ(ang3);
+	M = Translate(0, 0.2, 0) * Scale(0.1, 0.4, 0.1);
+	glUniformMatrix4fv(uMat, 1, true, CTM * M);
+	cube.Draw(program);
+
+	MStack.push_back(CTM);
+
+	MStack.pop_back();
+	CTM = MStack.back();
+	MStack.pop_back();
+
+	//Hand2
+	CTM *= Translate(-0.2, 0.1, 0) * RotateZ(ang3);
+	M = Translate(0, 0.2, 0) * Scale(0.1, 0.4, 0.1);
+	glUniformMatrix4fv(uMat, 1, true, CTM * M);
+	cube.Draw(program);
+
+	MStack.push_back(CTM);
+	//Hand1 - middle
+	CTM *= Translate(0.4, 0.4, 0);
+	M = Translate(0, 0.05, 0) * Scale(0.4, 0.1, 0.1);
+	glUniformMatrix4fv(uMat, 1, true, CTM * M);
+	cube.Draw(program);
+
+	MStack.push_back(CTM);
+
+	//Hand1 - right
+	CTM *= Translate(0.2, 0.1, 0);
+	M = Translate(0, 0.05, 0) * Scale(0.1, 0.4, 0.1);
+	glUniformMatrix4fv(uMat, 1, true, CTM * M);
+	cube.Draw(program);
+
+	MStack.push_back(CTM);
+	MStack.pop_back();
+	CTM = MStack.back();
+	MStack.pop_back();
+	//Hand1 - left
+	CTM *= Translate(-0.2, 0.1, 0);
+	M = Translate(0, 0.05, 0) * Scale(0.1, 0.4, 0.1);
+	glUniformMatrix4fv(uMat, 1, true, CTM * M);
+	cube.Draw(program);
+}
 void drawWindmill(float angle)
 {
 	GLuint uMat = glGetUniformLocation(program, "uMat");
@@ -58,38 +165,7 @@ void myDisplay()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glEnable(GL_DEPTH_TEST);
 
-	GLuint uMat = glGetUniformLocation(program, "uMat");
-
-	mat4 M(1.0);
-	mat4 CTM(1.0);
-
-	float ang1 = 30 * sin(g_Time * 2);
-	float ang2 = 30 * sin(g_Time* 4);
-	float ang3 = -30 * sin(g_Time*8);
-
-	//Base
-	CTM = Translate(0, -0.5, 0);
-	M = Scale(0.3, 0.2, 0.2);
-	glUniformMatrix4fv(uMat, 1, true, CTM * M);
-	pyramid.Draw(program);
-
-	//Uupper Arm
-	CTM *= RotateZ(ang1);
-	M = Translate(0,0.2,0)*Scale(0.1, 0.4, 0.1);
-	glUniformMatrix4fv(uMat, 1, true, CTM * M);
-	cube.Draw(program);
-
-	//Lower Arm
-	CTM *= Translate(0, 0.4, 0) * RotateZ(ang2);
-	M = Translate(0, 0.2, 0) * Scale(0.1, 0.4, 0.1);
-	glUniformMatrix4fv(uMat, 1, true, CTM * M);
-	cube.Draw(program);
-
-	//Hand
-	CTM *= Translate(0, 0.4, 0) * RotateZ(ang3);
-	M =  Translate(0, 0.05, 0)*Scale(0.2, 0.1, 0.1);
-	glUniformMatrix4fv(uMat, 1, true, CTM * M);
-	cube.Draw(program);
+	drawRobotArm2(30 * sin(g_Time), 45 * sin(g_Time ), 40 * sin(g_Time));
 
 	glutSwapBuffers();
 }
